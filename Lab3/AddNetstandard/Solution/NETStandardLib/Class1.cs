504cd8358346e9fafa62e8cf457f8ff7a6c6792b
Fion <fiondev7@gmail.com>
2018-01-21 14:47:46 +0800

    在NetStandardLib中針對不同NetFramework加入特定參考

--- Lab3/AddNetstandard/Solution/NETStandardLib/Class1.cs
+++ Lab3/AddNetstandard/Solution/NETStandardLib/Class1.cs
@@ -6,10 +6,13 @@ namespace NETStandardLib
     {
         public string Hello()
         {
-            #if NET40
+#if NET40
             return "Hi NET40";
-            #elif NET45
+#elif NET45
+            new System.Net.WebClient();
+            new System.Net.Http.HttpClient();//改cs project 
             return "Hi NET45";
+
             #elif NETCOREAPP1_0
             return "Hi NETCOREAPP1_0";
             #elif NETCOREAPP2_0
